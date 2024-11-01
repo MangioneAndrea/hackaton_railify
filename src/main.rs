e2107f073aaec043ff15191362014344e3b94696
Nicola Distl <nicola.m.distl@gmail.com>
2024-11-01 23:47:31 +0100

    draw circles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use image::{Rgba, RgbaImage};
+use imageproc::drawing::draw_hollow_circle_mut;
+
+enum EasyColor {
+    Red,
+    Green,
+}
+
+struct CircleCoordinates {
+    x: i32,
+    y: i32,
+    radius: i32,
+    width: u8,
+    color: EasyColor,
+}
+
+fn draw_circles(img: &mut RgbaImage, circle_coordinates_list: &[CircleCoordinates]) {
+    for circle_coordinates in circle_coordinates_list {
+        let color_rgba = match circle_coordinates.color {
+            EasyColor::Green => Rgba([0, 255, 0, 100]),
+            EasyColor::Red => Rgba([255, 0, 0, 100]),
+        };
+
+        for i in 0..circle_coordinates.width {
+            draw_hollow_circle_mut(
+                img,
+                (circle_coordinates.x, circle_coordinates.y),
+                circle_coordinates.radius + i as i32,
+                color_rgba,
+            );
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = image::open("./src/cat.jpg")
+        .expect("Failed to open image")
+        .to_rgba8();
+
+    let circles = vec![
+        CircleCoordinates {
+            x: 100,
+            y: 150,
+            radius: 50,
+            width: 3,
+            color: EasyColor::Red,
+        },
+        CircleCoordinates {
+            x: 200,
+            y: 100,
+            radius: 30,
+            width: 2,
+            color: EasyColor::Green,
+        },
+        CircleCoordinates {
+            x: 200,
+            y: 300,
+            radius: 10,
+            width: 1,
+            color: EasyColor::Green,
+        },
+    ];
+    draw_circles(&mut img, &circles);
+
+    img.save("output.png").expect("Failed to save image");
 }
