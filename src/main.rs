680aa265d354d8268e8223bc84a92606deefccb3
Andrea Mangione <andrea@mangione.dev>
2024-11-01 23:30:41 +0100

    feat: add reading of pdf printing just the first pixel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use image::RgbImage;
+use pdfium_render::prelude::*;
+use std::path::{Path, PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Page number
+    #[arg(short, long, default_value_t = 0)]
+    page: u32,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let images = pdf_images(&args.input, None)?;
+
+    dbg!(images[0][(0,0)]);
+
+    Ok(())
+}
+
+fn pdf_images(
+    path: &impl AsRef<Path>,
+    password: Option<&str>,
+) -> Result<Vec<RgbImage>, PdfiumError> {
+    let pdfium = Pdfium::default();
+
+    let document = pdfium.load_pdf_from_file(path, password)?;
+
+    let render_config = PdfRenderConfig::new()
+        .set_target_width(2000)
+        .set_maximum_height(2000)
+        .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
+
+    let mut images = vec![];
+    for (_, page) in document.pages().iter().enumerate() {
+        images.push(
+            page.render_with_config(&render_config)?
+                .as_image()
+                .into_rgb8(),
+        );
+    }
+
+    Ok(images)
 }
