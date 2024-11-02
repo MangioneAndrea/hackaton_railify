60b34ea3b7c335366d425ffff654025be8f4d172
Gabriele Mangione <Gabriele.Mangione@hotmail.com>
2024-11-02 18:16:07 +0100

    Update data_structures.rs

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -12,33 +12,56 @@ struct Line {
     end: (i32, i32),
 }
 
-#[derive(Clone)]
 enum Connectable {
     Node(Rc<Node>),
     Label(Rc<Label>),
 }
 
-trait Coordinator {
-    fn get_coordinates(&self) -> (i32, i32);
+impl Connectable {
+    fn get_coordinates(&self) -> (i32, i32) {
+        match self {
+            Connectable::Node(a) => a.coordinates,
+            Connectable::Label(a) => a.coordinates,
+        }
+    }
 }
 
 struct Label {
-    coordinates: (i32,i32),
+    coordinates: (i32, i32),
     prev: Connectable,
+    label: String,
 }
 
-struct Node{
+trait SingleLiner {
+    fn get_line(&self) -> Line;
+}
+
+impl SingleLiner for Label {
+    fn get_line(&self) -> Line {
+        Line {
+            start: self.coordinates,
+            end: self.prev.get_coordinates(),
+        }
+    }
+}
+impl Label {
+    fn get_text(&self) -> String {
+        self.label.clone()
+    }
+}
+
+struct Node {
     coordinates: (i32, i32),
     prev: Vec<Connectable>,
 }
 
-impl Node{
+impl Node {
     fn get_lines(&self) -> Vec<Line> {
         let mut v = Vec::<Line>::new();
-        for n in self.prev.clone() {
+        for n in self.prev.iter() {
             v.push(Line {
                 start: self.coordinates,
-                end: n.coordinates,
+                end: n.get_coordinates(),
             });
         }
         v
@@ -52,11 +75,27 @@ impl Node{
 }
 
 pub fn example() {
-    let node_a = Rc::new(Node {
+    let new_node = Rc::new(Node {
+        coordinates: (1, 1),
+        prev: Vec::new(),
+    });
+
+    let new_label = Rc::new(Label {
+        coordinates: (2, 1),
+        prev: Connectable::Node(new_node.clone()),
+        label: "Hello".to_string()
+    });
+
+    println!("{:?}", new_node.get_circle());
+    println!("{:?}", new_node.get_lines());
+    println!("{:?}", new_label.get_line());
+    println!("{:?}", new_label.get_text());
+    /*
+    let node_a = Rc::new(Connectable{Node(Node {
         coordinates: (1, 1),
         nody: Nody::Intersection,
         prev_nodes: Vec::new(),
-    });
+    })});
     let node_b = Rc::new(Node {
         coordinates: (1, 2),
         nody: Nody::Intersection,
@@ -95,15 +134,16 @@ pub fn example() {
         prev_nodes: vec![node_g.clone()],
     });
 
+    */
     /*
     for l in node_h.get_lines() {
         println!("{:?}", l);
 
     }
-    */
 
     println!("{:?}", node_h.get_circle());
     println!("{:?}", node_g.get_circle());
     println!("{:?}", node_g.get_lines());
     println!("{:?}", node_e.get_lines());
+    */
 }
