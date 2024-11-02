44de0455a4a2255c18dd90872f52cfaec4378ff3
Xeno Suter <64072009+xeno-suter@users.noreply.github.com>
2024-11-02 10:35:20 +0100

    extrac draw lib and add draw components

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use nannou::prelude::*;
+use draw::*;
 
 use image::{Rgb};
 use imageproc::drawing::draw_hollow_circle_mut;
@@ -23,32 +23,6 @@ struct CircleCoordinates {
     radius: i32,
     width: u8,
     color: EasyColor,
-}
-
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
-}
-
-fn event(_app: &App, _model: &mut Model, _event: Event) {}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
-    let draw = app.draw();
-
-    let sine = app.time.sin();
-    let slowersine = (app.time / 2.0).sin();
-
-    let boundary = app.window_rect();
-
-    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
-    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom(), boundary.top());
-
-    draw.background().color(PLUM);
-
-    draw.ellipse().color(STEELBLUE).x_y(x, y);
-
-    draw.to_frame(app, &frame).unwrap();
 }
 
 fn draw_circles(img: &mut RgbImage, circle_coordinates_list: &[CircleCoordinates]) {
@@ -116,7 +90,7 @@ fn main() -> anyhow::Result<()> {
     let svg = convert_image_to_svg(image);
     svg::save("output.svg", &svg).expect("Failed to save svg");
 
-    nannou::app(model).event(event).simple_window(view).run();
+    nannou::app(model).update(update).simple_window(view).run();
 
     dbg!(images[0][(0, 0)]);
 
